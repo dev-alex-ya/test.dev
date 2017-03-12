27d1180a9a00c2e33e7eb97f620030fcd2303441
Yarymokha Alexandr <ya.alexandr.86@gmail.com>
2017-03-12 13:01:12 +0200

    add controllers functionality

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,10 @@
 <?php
-$app->get('/', function(){return 'index';});
+/*$app->get('/', function(){return 'index';});
+
 $app->get('/home', function($request, $response){
     return $this->view->render($response, 'home.twig');
 
-});
+});*/
+
+$app->get('/', 'HomeController:index');
+$app->get('/home', 'HomeController:home');
