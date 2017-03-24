50a682c41c7fba36d7cc0750323bb8a717a75db0
alex <ya.alexandr.86@gmail.com>
2017-03-24 14:42:51 +0200

    добавлены модели. Настроена ORM

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,6 +5,7 @@
 */
 namespace App\Controllers;
 
+use App\Models\User;
 use Slim\Views\Twig as View;
 
 
@@ -12,6 +13,18 @@ class HomeController extends Controller
 {
 	public function index($request, $response)
 	{
+
+		/*$user = User::where('name', 'testname')->first();
+		var_dump($user->password);*/
+
+/*//метод добавления в базу данных
+		User::create([
+			'name'=>'ukrsalo',
+			'email'=>'ukrsalo@pochta.ru',
+			'password'=>'123'
+		]);
+*/
+
 		return $this->view->render($response, 'home.twig');
 	}
 }
